21446d96e8b3d48a15d4ecd0c59c4914e089d47b
Joseph Maldjian <josephavm@icloud.com>
2022-12-01 21:24:23 -0600

    Switch to using table props

--- app/instances/page.tsx
+++ app/instances/page.tsx
@@ -1,13 +1,25 @@
 import Table from "../../components/Table";
 
 export default function Home() {
+    function instanceFormatter(name) {
+        return [
+            <div className="text-base font-semibold">{name}</div>,
+            "Live Data Here?",
+            <div className="flex items-center">
+                <div className="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> Online
+            </div>,
+            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit Instance</a>
+
+        ]
+    }
     return (
-      <>
-          <h1 className="text-3xl dark:text-white font-serif mb-5">
-       Instances
-      </h1>
-      <Table />
-      </>
+        <>
+            <h1 className="text-3xl dark:text-white font-serif mb-5">
+                Instances
+            </h1>
+            <Table 
+            columns={["Name", "Resource Usage", "Status", "Edit"]}
+            rows={[instanceFormatter("Express Server"), instanceFormatter("Web Server"), instanceFormatter("Development")]} />
+        </>
     )
-  }
-  
+}
