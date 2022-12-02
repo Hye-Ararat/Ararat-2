c399e81c2ae99aff91ff43fda7b7a92c63dc5248
Joseph Maldjian <josephavm@icloud.com>
2022-12-01 19:51:59 -0600

    Instance Table

---
+++ app/instances/page.tsx
@@ -0,0 +1,13 @@
+import Table from "../../components/Table";
+
+export default function Home() {
+    return (
+      <>
+          <h1 className="text-3xl dark:text-white font-serif mb-5">
+       Instances
+      </h1>
+      <Table />
+      </>
+    )
+  }
+  
