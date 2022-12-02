a08ab96a79dd91365c64328c4c8b12b0474cd695
Joseph Maldjian <josephavm@icloud.com>
2022-12-01 19:51:42 -0600

    Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-  ], 
-   theme: {
-    extend: {},
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["var(--font-poppins)", ...fontFamily.serif],
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      }
+    },
+  colors: {
+    dark: "#0d141d"
+  }
   },
-  plugins: [],
+  darkMode: "media",
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
