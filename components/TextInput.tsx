f2b4ba439fa34588cf721c3be2ed3529afc99382
Wolfo Gaming <klokko06@gmail.com>
2022-12-14 17:43:43 +0000

    Co-authored-by: Joseph Maldjian <josephavm@icloud.com>

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,5 +1,6 @@
-export default function TextInput({placeholder, type, id, required}) {
+
+export default function TextInput({placeholder, type, id, required}:{placeholder:any, type:any,id:any,required:any}) {
     return (
-        <input type={type} id={id} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required={required} />
+        <input type={type} id={id} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required={required} />
     )
 }
