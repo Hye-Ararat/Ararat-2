b870087881a7531f6380fb9c38a86a97da26a562
Joseph Maldjian <joseph.maldjian@hye.gg>
2022-12-14 17:38:42 +0000

    Fix CSS issues

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,7 +5,7 @@ import TextInput from "../../../components/TextInput";
 export default function Login() {
     return (
         <>
-        <div class="container mx-auto">
+        <div className="container mx-auto">
         <img className="h-20 rounded-lg m-auto mt-64" src="https://cloud.hye.gg/logo.png" alt="image description" />
         <p className="text-sm dark:text-white text-center mt-4 mb-3.5">Welcome to Ararat! Please login with your account.</p>
 <form className="flex flex-col gap-4 w-1/4 mx-auto mt-30">
