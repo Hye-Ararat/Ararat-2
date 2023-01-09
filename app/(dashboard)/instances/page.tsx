09ebcb0d275edb54c99d6a4ab1769002a9f1c58a
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-01-09 15:20:28 +0000

    Instances Page

--- app/(dashboard)/instances/page.tsx
+++ app/(dashboard)/instances/page.tsx
@@ -1,17 +1,32 @@
-    import Table from "../../../components/Table";
+import Link from "next/link";
+import Table from "../../../components/Table";
+import {HyeXD} from "../../../lib/hyexd"
+import prisma from "../../../lib/prisma";
 
-export default function Home() {
-    function instanceFormatter(name: string) {
+export default async function Home() {
+    function instanceFormatter(name: string, id: string, nodeUrl: string) {
         return [
+            <Link href={nodeUrl + `/instances/${id}`}>
             <div className="text-base font-semibold">{name}</div>,
             "Live Data Here?",
             <div className="flex items-center">
                 <div className="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div> Online
             </div>,
             <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit Instance</a>
-
+            </Link>
         ]
     }
+    let instances = await prisma.instance.findMany({
+        include: {
+            node: true
+        }
+    })
+
+    let tableData : (string|JSX.Element)[][] = [];
+
+    instances.forEach((instance) => {
+        tableData.push(instanceFormatter(instance.name, instance.id, instance.node.url))
+    })
     return (
         <>
             <h1 className="text-3xl dark:text-white font-serif mb-5">
@@ -19,7 +34,7 @@ export default function Home() {
             </h1>
             <Table 
             columns={["Name", "Resource Usage", "Status", "Edit"]}
-            rows={[instanceFormatter("Express Server"), instanceFormatter("Web Server"), instanceFormatter("Development")]} />
+            rows={tableData} />
         </>
     )
 }
