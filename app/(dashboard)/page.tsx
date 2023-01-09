419e5f45340c3ab0bd97845eb9e87ad5e99432cd
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-01-09 15:20:11 +0000

    Dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,9 +3,8 @@ export default function Home() {
   return (
     <>
         <h1 className="text-3xl font-bold dark:text-white font-serif">
-      Hello world!
+      Dashboard
     </h1>
-    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
     </>
   )
 }
