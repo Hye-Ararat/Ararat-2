9d7190e114a67c0e8399a4304982ce3c41647e8c
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-01-10 19:38:57 +0000

    Download LXD

--- cli/index.js
+++ cli/index.js
@@ -85,11 +85,11 @@ yargs.scriptName("ararat").usage("$0 <cmd> [args]")
     })
     .command("setupnode", "Sets up node (internal command)", () => { }, async () => {
         console.log("Installing Dependency: caddy...");
-        execSync("apt-get install -y debian-keyring debian-archive-keyring apt-transport-https");
-        execSync("curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg");
-        execSync("curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list");
-        execSync("sudo apt-get update -y")
-        execSync("sudo apt-get install caddy -y")
+        execSync("apt-get install -y debian-keyring debian-archive-keyring apt-transport-https wget");
+        execSync("curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --batch --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg");
+        execSync("curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list");
+        execSync("apt-get update -y")
+        execSync("apt-get install caddy -y")
         console.log("✅ Dependency caddy successfully installed")
         let useDomain = await prompts({
             type: "confirm",
@@ -210,7 +210,7 @@ Requires=network.target
 [Service]
 Type=notify
 WorkingDirectory=/var/lib/cockroach
-ExecStart=/usr/local/bin/cockroach start --certs-dir=certs --advertise-addr=${address.value} --join=${address.value} --cache=.25 --max-sql-memory=.25
+ExecStart=/usr/local/bin/cockroach start --certs-dir=certs --advertise-addr=${address.value} --join=${address.value} --http-addr=127.0.0.1:8081 --cache=.25 --max-sql-memory=.25
 TimeoutStopSec=300
 Restart=always
 RestartSec=10
@@ -243,8 +243,120 @@ WantedBy=default.target
         name: "value",
         message: "What should the password to this database account be?"
     })
-    //for tomorrow
-    //let cockroachSql = execSync(`cockroach sql --certs-dir=/var/lib/cockroach/certs --host=development.hye.gg --execute"CREATE USER ${dbUsername.value} WITH PASSWORD '${dbPassword.value}'"`);
+    console.log("Creating Database User...");
+    execSync(`cockroach sql --certs-dir=/var/lib/cockroach/certs --host=development.hye.gg --execute="CREATE USER ${dbUsername.value} WITH PASSWORD '${dbPassword.value}'";`);
+    console.log("✅ Database User Created")
+    console.log("Granting Administrative Privileges...");
+    execSync(`cockroach sql --certs-dir=/var/lib/cockroach/certs --host=development.hye.gg --execute="GRANT admin TO ${dbUsername.value}";`);
+    console.log("✅ Administrative Privileges Granted")
+    console.log("Creating Database...");
+    execSync(`cockroach sql --certs-dir=/var/lib/cockroach/certs --host=development.hye.gg --execute="CREATE DATABASE ararat";`);
+    console.log("✅ Database Created")
+    console.log("Saving Database Configuration...");
+    let envConfig = "";
+    envConfig += `DATABASE_URL="postgresql://${dbUsername.value}:${dbPassword.value}@127.0.0.1:26257/ararat"`
+    writeFileSync(".env", envConfig);
+    console.log("✅ Database Configuration Saved");
+    console.log("Seeding Database...")
+    execSync("npx prisma db push");
+    console.log("✅ Database Seeded")
+    console.log("Installing dependency: snapd...");
+    execSync("apt-get install -y snapd")
+    console.log("✅ Installed dependency snapd")
+    console.log("Installing dependency: LXD...")
+    execSync("snap install lxd");
+    console.log("✅ Installed dependency LXD")
+    console.log("Patching LXD...");
+    try {
+        execSync("rm lib/lxd_amd64.snap")
+    } catch {
+        
+    }
+    execSync("wget https://github.com/Hye-Ararat/lxd-pkg-snap/releases/download/5.9_amd64/lxd_5.9_amd64.snap -O lib/lxd_amd64.snap")
+    execSync("snap install lib/lxd_amd64.snap --dangerous");
+    console.log("✅ Patched LXD")
+    console.log("Generating Encryption Key...");
+    const randomString = () => {
+        const length = 32;
+        const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        let result = "";
+        for (let i = 0; i < length; i++) {
+            result += charset.charAt(Math.floor(Math.random() * charset.length));
+        }
+        return result;
+    }
+    let envLocal = "";
+    envLocal += `ENC_KEY=${randomString()}\n`;
+    console.log("✅ Encryption Key Generated")
+    console.log("Generating Communication Key...")
+    envLocal += `COMMUNICATION_KEY=${randomString()}\n`;
+    console.log("✅ Communication Key Generated")
+    console.log("Saving Configuration...");
+    writeFileSync(".env.local", envLocal);
+    console.log("✅ Configuration Saved!")
+    console.log("Building Ararat...");
+    execSync("npm run build");
+    console.log("✅ Build Successful")
+    console.log("Adjusting Permissions...")
+    execSync("chmod +x start.sh");
+    console.log("✅ Permissions Adjusted")
+    console.log("Creating System Service...")
+    try {
+        execSync("systemctl stop ararat");
+        execSync("rm /etc/systemd/system/ararat.service");
+    } catch (error) {
+        
+    }
+    let araratSystemd = `
+[Unit]
+After=network.target
+Description=Hye Ararat
+    
+[Service]
+WorkingDirectory=/usr/lib/ararat/
+Environment="NODE_ENV=production"
+ExecStart=/usr/lib/ararat/start.sh
+Restart=on-failure
+User=root
+    
+[Install]
+WantedBy=multi-user.target
+`
+    writeFileSync("/etc/systemd/system/ararat.service", araratSystemd);
+    console.log("✅ System Service Created")
+    console.log("Enabling and Starting System Service...");
+    execSync("systemctl enable ararat");
+    execSync("systemctl start ararat");
+    console.log("✅ Ararat enabled at startup and now running")
+    console.log("Now let's make a Hye Ararat account");
+    const username = await prompts({
+        message: "What would you like your username to be",
+        name: "value",
+        type: "text"
+    })
+    const password = await prompts({
+        message: "What would you like your password to be",
+        name: "value",
+        type: "password"
+    })
+    const email = await prompts({
+        message: "What is your email address",
+        name: "value",
+        type: "text"
+    })
+    const firstName = await prompts({
+        type: "text",
+        name: "value",
+        message: "What is your first name"
+    })
+    const lastName = await prompts({
+        type: "text",
+        name: "value",
+        message: "What is your last name"
+    })
+    console.log("Creating user...");
+    execSync(`ararat user:create --firstName=${firstName.value} --lastName=${lastName.value} --email=${email.value} --password=${password.value} --username=${username.value} --language=english`)
+    console.log("✅ User Created")
     console.log("You're node has been setup! You can now navigate to it using the URL you specified earlier in your web browser.")
     }
     )
