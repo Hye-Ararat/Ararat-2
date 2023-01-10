4a5c1ecfa084e022fe5a6d76bc7b3742f033f896
Joseph Maldjian <joseph.maldjian@hye.gg>
2023-01-10 18:39:43 +0000

    Fix types

--- components/Stepper.tsx
+++ components/Stepper.tsx
@@ -1,4 +1,9 @@
-export default function Stepper({steps}) {
+interface Step {
+    name: string,
+    complete: boolean
+}
+
+export default function Stepper({steps} : {steps: Step[]}) {
     return (
 <ol className="flex items-center w-full p-3 space-x-2 text-sm font-medium text-center text-gray-500 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 sm:text-base dark:bg-gray-800 dark:border-gray-700 sm:p-4 sm:space-x-4">       
  {steps.map((step, index) => {
@@ -9,7 +14,7 @@ export default function Stepper({steps}) {
                     {index + 1}
                 </span>
                 {step.name}
-                <svg aria-hidden="true" class="w-4 h-4 ml-2 sm:ml-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg>
+                <svg aria-hidden="true" className="w-4 h-4 ml-2 sm:ml-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg>
             </li>
     :    <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
     <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500">
